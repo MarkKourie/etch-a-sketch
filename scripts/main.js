a54ac5d3ecc8e1ac45fac0b45db9fb5f5387220a
Megalopsychic <mfkourie@gmail.com>
2019-12-18 20:04:46 +0000

    added event listener logic to create etch-a-sketch. uses full color and darkens over each pass. resets to new colour after lightness < 10

--- scripts/main.js
+++ scripts/main.js
@@ -18,5 +18,21 @@ function setupGrid() {
 
 setupGrid();
 
+var items = document.querySelectorAll('.item');
 
+items.forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+        if ((item.getAttribute('style') == null) 
+            || (item.getAttribute('style').slice(-5, -3) < 10)) { 
+        item.setAttribute("style", `background-color: 
+        hsl(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 101)}%, ${Math.floor(Math.random() * 101)}%);`);
+        console.log(item.getAttribute('style').slice(-5, -3))
+        } else {
+            item.setAttribute("style", 
+                `${item.getAttribute('style').slice(0, -6)} ${(item.getAttribute('style').slice(-5, -3) - 10)}%);`);
+            console.log(item.getAttribute('style'))
+        }
+    })
+
+})
 
