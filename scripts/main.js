fb8f39811dac651d0ed597b470b87cb5cbcbb796
Megalopsychic <mfkourie@gmail.com>
2019-12-19 11:56:24 +0000

    added resize option. Refactored code into neater functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,13 @@
-//setting up the grid container
+window.onload = function() {
+    document.getElementById('number-of-blocks').value = 16;
+    setupGrid();
+    colorChangeBehaviour();
+}
+
 const container = document.querySelector('#container');
 
 function setupGrid() {
-    const numberOfBlocks = parseInt(prompt("Choose number of rows/columns in the grid", '16'));
+    const numberOfBlocks = getNumberFromInput();
     var fragment = document.createDocumentFragment();
 
     for (i = 0; i < numberOfBlocks**2; i++) {
@@ -18,24 +23,20 @@ function addGridAttribute(element, numberOfColumnsAndRows) {
     element.setAttribute('style', `grid-template-columns: repeat(${numberOfColumnsAndRows}, minmax(1px, 1fr)); grid-template-rows: repeat(${numberOfColumnsAndRows}, minmax(1px, 1fr))`)
 }
 
-//execute grid setup
-setupGrid();
-
-//the code that determines behaviour of grid items
-var items = document.querySelectorAll('.item');
-
-items.forEach((item) => {
-    item.addEventListener('mouseenter', (e) => {
-        if (item.getAttribute('style') == null) { 
-            randomiseHSLColor(item);
-        } else if (item.getAttribute('style').slice(-5, -3) >= 10) {
-            removeTenLightness(item);
-        } else {
-            paintedBlack(item);
-        }
+function colorChangeBehaviour() {
+    var items = document.querySelectorAll('.item');
+    items.forEach((item) => {
+        item.addEventListener('mouseenter', (e) => {
+            if (item.getAttribute('style') == null) { 
+                randomiseHSLColor(item);
+            } else if (item.getAttribute('style').slice(-5, -3) >= 10) {
+                removeTenLightness(item);
+            } else {
+                paintedBlack(item);
+            }
+        })
     })
-
-})
+}
 
 function randomiseHSLColor(element) {
     element.setAttribute('style', `background-color: hsl(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 101)}%, ${Math.floor(Math.random() * 101)}%);`);
@@ -49,17 +50,29 @@ function paintedBlack(element) {
     element.setAttribute('style', 'background-color: hsl(000, 0%, 0%');
 }
 
-//reset button
-var resetButton = document.querySelector('#reset');
-
-resetButton.addEventListener('click', (e) => {
+function removeStyle() {
+    var items = document.querySelectorAll('.item');
     items.forEach((item) => {
         item.removeAttribute('style');
     })
+}
+
+function getNumberFromInput() {
+   return document.getElementById("number-of-blocks").value;
+}
+
+//buttons
+var resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', (e) => removeStyle());
+
+var resizeButton = document.querySelector('#resize');
+resizeButton.addEventListener('click', (e) => {
+    setupGrid();
+    colorChangeBehaviour();
+    removeStyle();
 });
 
 // Still to do:
-// 1. Add resize button rather than simple prompt on load
 // 2. Add colour option toggles
 // 3. Add hold to paint toggle (off means mouseenter, on means mouse hold)
 // 4. Structure layout with CSS - make sketchpad centred, buttons in a nav, nice background, etc.
